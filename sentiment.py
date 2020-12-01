883cfdbd4ff2ffed2309c63b4b46cadefb6648bb
blporter <brookeporter13@gmail.com>
2020-11-30 23:19:51 -0800

    Add training data

--- sentiment.py
+++ sentiment.py
@@ -3,40 +3,54 @@ import ssl
 import csv
 import re
 import string
+import random
+
 from nltk.tokenize import TweetTokenizer
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk import classify
+from nltk import NaiveBayesClassifier
 
-fieldnames = ['user_id', 'stat_id', 'creation', 'tweet_body', 'name']
+tweet_fields = ['user_id', 'stat_id', 'creation', 'tweet_body', 'name']
+training_fields = ['target', 'ids', 'date', 'flag', 'user', 'text']
+positive_tweets = []
+negative_tweets = []
 
 
 class SentimentAnalysis:
-    content = []
-    cleaned_content = []
-
     def __init__(self):
         super(SentimentAnalysis, self).__init__()
 
-    def tokenize_csv(self):
-        print("Tokenizing.")
+    def tokenize_csv(self, file_path):
+        print("Tokenizing csv file")
         tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
-        with open('data/tweets.csv', newline='') as file:
-            reader = csv.DictReader(file, fieldnames=fieldnames)
+        with open(file_path, newline='') as file:
+            reader = csv.DictReader(file, fieldnames=tweet_fields)
+            content = []
             for row in reader:
-                self.content.append(tokenizer.tokenize(row['tweet_body']))
+                content.append(tokenizer.tokenize(row['tweet_body']))
+            return content
 
-    def clean_content(self):
-        print("Lemmatizing and cleaning.")
+    def tokenize_training_model(self, tweets):
+        print("Tokenizing training model")
+        tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
+        content = []
+        for row in tweets:
+            content.append(tokenizer.tokenize(row))
+        return content
+
+    def clean_content(self, content):
+        print("Lemmatizing and cleaning")
         lemmatizer = WordNetLemmatizer()
         stop_words = stopwords.words("english")
-        for row in self.content:
+        cleaned_content = []
+        for row in content:
             cleaned_sentence = []
             for word, tag in pos_tag(row):
                 # remove links (http(s) and everything after)
                 word = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '',
                               word)
-                # word = re.sub("(@[A-Za-z0-9_]+)", "", word)
 
                 if tag.startswith('NN'):
                     pos = 'n'
@@ -49,7 +63,12 @@ class SentimentAnalysis:
                 if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
                     cleaned_sentence.append(token.lower())
 
-            self.cleaned_content.append(cleaned_sentence)
+            cleaned_content.append(cleaned_sentence)
+        return cleaned_content
+
+    def prepare_content_for_model(self, cleaned_content):
+        for tokens in cleaned_content:
+            yield dict([token, True] for token in tokens)
 
 
 def download_nltk_libraries():
@@ -63,14 +82,52 @@ def download_nltk_libraries():
     print("NLTK download complete.")
 
 
+# My training dataset from Kaggle is too large to commit to github. Download the dataset from the following link:
+# https://www.kaggle.com/kazanova/sentiment140
+# and store it in the data folder.
+def split_training_file():
+    print("Splitting training file into positive and negative")
+    with open('data/training.1600000.processed.noemoticon.csv', newline='', encoding='latin-1') as file:
+        reader = csv.DictReader(file, fieldnames=training_fields)
+        for row in reader:
+            if int(row['target']) < 2:
+                negative_tweets.append(row['text'])
+            else:
+                positive_tweets.append(row['text'])
+
+
 def main():
     # download_nltk_libraries()
-
     analysis = SentimentAnalysis()
-    analysis.tokenize_csv()
-    analysis.clean_content()
 
-    print(analysis.cleaned_content)
+    split_training_file()
+
+    positive_tokens = analysis.tokenize_training_model(positive_tweets)
+    negative_tokens = analysis.tokenize_training_model(negative_tweets)
+    cleaned_positive_content = analysis.clean_content(positive_tokens)
+    cleaned_negative_content = analysis.clean_content(negative_tokens)
+    positive_content_for_model = analysis.prepare_content_for_model(cleaned_positive_content)
+    negative_content_for_model = analysis.prepare_content_for_model(cleaned_negative_content)
+
+    positive_dataset = [(tweet_dict, "Positive")
+                        for tweet_dict in positive_content_for_model]
+
+    negative_dataset = [(tweet_dict, "Negative")
+                        for tweet_dict in negative_content_for_model]
+
+    dataset = positive_dataset + negative_dataset
+    random.shuffle(dataset)
+
+    # train the first 70%, test the last 30%. We have 1.6 million tweets in our training data.
+    train_data = dataset[:1120000]
+    test_data = dataset[1120000:]
+
+    print("Training using dataset")
+    classifier = NaiveBayesClassifier.train(train_data)
+
+    print("Accuracy is:", classify.accuracy(classifier, test_data))
+
+    print(classifier.show_most_informative_features(10))
     return 0
 
 
